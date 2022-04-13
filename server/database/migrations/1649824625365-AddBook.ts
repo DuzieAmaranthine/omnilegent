4e9f4d8da92ffd272d4cbc7c3312b6b8b5165eec
skal-chin-wsl <chin.allen9@hotmail.com>
2022-04-13 12:15:06 -0600

    Finished AddBook table. Created api folder with book and chatroom controllers.

--- server/database/migrations/1649824625365-AddBook.ts
+++ server/database/migrations/1649824625365-AddBook.ts
@@ -1,11 +1,60 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class AddBook1649824625365 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name : 'book',
+				columns : [
+					{
+						name : 'id',
+						type : 'text',
+						isPrimary : true,
+						isGenerated : false,
+					},
+					{
+						name : 'title',
+						type : 'text',
+						isNullable : true,
+					},
+					{
+						name : 'author',
+						type : 'text',
+						isNullable : true,
+					},
+					{
+						name : 'description',
+						type : 'text',
+						isNullable : true,
+					},
+					{
+						name : 'pages',
+						type : 'int',
+						isNullable : true,
+					},
+					{
+						name : 'pubDate',
+						type : 'text',
+						isNullable : true,
+					},
+					{
+						name : 'thumbnail',
+						type : 'text',
+						isNullable : true,
+					},
+					{
+						name : 'hasRead',
+						type : 'boolean',
+						isNullable : false,
+					},
+				],
+			}),
+		);
+	}
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable('book');
+	}
 
 }
