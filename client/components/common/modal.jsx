0b6534c7f6c9ebbe38eb7c365118ab589510c199
DuzieAmaranthine <jessycalynn9@gmail.com>
2022-05-01 13:31:00 -0600

    Working on modal to search and add books

--- client/components/common/modal.jsx
+++ client/components/common/modal.jsx
@@ -14,15 +14,33 @@ export const Modal = ({ }) => {
               <div className="search">Search...</div>
               <div className="book-form">
                 <form>
-                  <label for="btitle">Title:</label>
-                  <input type="text" id="btitle" name="btitle" />
+                  <div className="input-fields">
+                    <label for="btitle">Title:</label>
+                    <input type="text" id="btitle" name="btitle" />
 
-                  <label for="bauthor">Author:</label>
-                  <input type="text" id="bauthor" name="bauthor" />
+                    <label for="bauthor">Author:</label>
+                    <input type="text" id="bauthor" name="bauthor" />
 
-                  <label for="bgenre">Genre:</label>
-                  <input type="text" id="bgenre" name="bgenre" />
+                    <label for="bgenre">Genre:</label>
+                    <input type="text" id="bgenre" name="bgenre" />
+                  
+                    <label for="bdescription">Description:</label>
+                    <input type="text" id="bdescription" name="bdescription" className="bdisc" />
+                  
+                  </div>
+                  
+                  <div>
+                    <span>Have You Read It Yet?</span>
+                    
+                    <label for="hasRead">Yes</label>
+                    <input type="radio" id="hasRead" name="has_read" value="Yes" />
 
+                    <label for="hasNotRead">No</label>
+                    <input type="radio" id="hasNotRead" name="has_read" value="No" />
+                    
+                    <label for="rdate">Date Read:</label>
+                    <input type="date" id="rdate" name="rdate" />
+                  </div>
                 </form>
               </div>
             </div>
